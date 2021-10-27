b4b566c2f678cb16481edb4f94403c3776428b8f
mdAshik922 <md.ashik0005723@gmail.com>
2021-10-27 23:50:17 +0600

    first commit

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,12 +1,5 @@
 const firebaseConfig = {
-  //   apiKey: "AIzaSyC70Kr9pelOn4FvXUxMt28hAubi2XxscE0",
-  //   authDomain: "genius-car-ab171.firebaseapp.com",
-  //   projectId: "genius-car-ab171",
-  //   storageBucket: "genius-car-ab171.appspot.com",
-  //   messagingSenderId: "802001312219",
-  //   appId: "1:802001312219:web:507459ab09794e1e8c2ffe",
-  //   measurementId: "G-V7JD1YL1KX"
-  // };
+
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
